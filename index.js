659b3b6c11b33063fb49a02a112d539b40118444
sayed8901 <sayed91515@gmail.com>
2023-03-15 22:43:08 +0600

    quiz questions loaded successfully.

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -59,22 +59,25 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+    // console.log(quiz, i);
+    quizContainer.innerHTML += 
+    `<div class="m-3 py-3 px-4 shadow-sm rounded">
+      <div class="flex items-center">
+        <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+          ${i + 1}
+        </div>
+        <p class="text-gray-800 text-sm">${quiz.question}</p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mt-5">
+        ${displayQuizOptions(quiz.options, i)}
+      </div>
+    </div>`;
   });
 };
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventlistener("click", () => {
+  console.log(answers.length);
   if (answers.length < 6) {
     return;
   }
