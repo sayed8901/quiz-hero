18be1cfd5e5e34535a79545e8481ad48fd0bcf73
sayed8901 <sayed91515@gmail.com>
2023-03-15 23:00:11 +0600

    Submit Quize button is working fine!

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
-  console.log(answers.length);
+document.querySelector("#submit").addEventListener("click", () => {
+  // console.log(answers.length);
   if (answers.length < 6) {
     return;
   }
